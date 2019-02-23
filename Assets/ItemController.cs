6472247e2b3b190b4c99b72abe568cfe8296fe42
KazuyaOomoto <n4p4yeah@gmail.com>
2019-02-23 17:12:12 +0900

    StepAsideUnityChan 初めてのコミット

---
+++ Assets/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+
+	//カメラのオブジェクト
+	private new GameObject camera;
+
+	// Use this for initialization
+	void Start(){
+		//カメラのオブジェクトを取得
+		camera = GameObject.Find ("Main Camera");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//画面外に出たら破棄する
+		if (this.transform.position.z <= this.camera.transform.position.z){
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemGenerator : MonoBehaviour {
+	//carPrefabを入れる
+	public GameObject carPrefab;
+	//coinPrefabを入れる
+	public GameObject coinPrefab;
+	//cornPrefabを入れる
+	public GameObject conePrefab;
+	//スタート地点
+	private int startPos;
+	//ゴール地点
+	private int goalPos = 120;
+	//アイテムを出すx方向の範囲
+	private float posRange = 3.4f;
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+	//アイテムを生成する間隔
+	private readonly int itemspace = 45;
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find ("unitychan");
+		this.startPos = (int)this.unitychan.transform.position.z + this.itemspace;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (((this.startPos - (int)this.unitychan.transform.position.z) <= this.itemspace) &&
+			(this.startPos < this.goalPos - this.itemspace)) {
+			//どのアイテムを出すのかをランダムに設定
+			int num = Random.Range (1, 11);
+			if (num <= 2) {
+				//コーンをx軸方向に一直線に生成
+				for (float j = -1; j <= 1; j += 0.4f) {
+					GameObject cone = Instantiate (conePrefab) as GameObject;
+					cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, this.startPos);
+				}
+			} else {
+				//レーンごとにアイテムを生成
+				for (int j = -1; j <= 1; j++) {
+					//アイテムの種類を決める
+					int item = Random.Range (1, 11);
+					//アイテムを置くZ座標のオフセットをランダム(40m～50m)に設定
+					int offsetZ = Random.Range (-5, 6);
+					//60%コイン配置:40%車配置:10%何もなし
+					if (1 <= item && item <= 6) {
+						//コインを生成
+						GameObject coin = Instantiate (coinPrefab) as GameObject;
+						coin.transform.position = new Vector3 (posRange * j, coin.transform.position.y, this.startPos + offsetZ);
+					} else if (7 <= item && item <= 9) {
+						//車を生成
+						GameObject car = Instantiate (carPrefab) as GameObject;
+						car.transform.position = new Vector3 (posRange * j, car.transform.position.y, this.startPos + offsetZ);
+					}
+				}
+			}
+			this.startPos += this.itemspace;
+		}
+	}
+}
